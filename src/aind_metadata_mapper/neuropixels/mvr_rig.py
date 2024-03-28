1b75e93a0c61c687d72bbbfc0865128fd019a183
Christopher <chrism@alleninstitute.org>
2024-03-28 09:42:07 -0700

    refactor mvr etl

--- src/aind_metadata_mapper/neuropixels/mvr_rig.py
+++ src/aind_metadata_mapper/neuropixels/mvr_rig.py
@@ -1,13 +1,16 @@
-import typing
+import logging
 import pathlib
 from aind_data_schema.core import rig  # type: ignore
 from aind_data_schema.models import devices  # type: ignore
-from . import neuropixels_rig, utils, NeuropixelsRigException
+from . import neuropixels_rig, utils
+
+
+logger = logging.getLogger(__name__)
 
 
 class ExtractContext(neuropixels_rig.NeuropixelsRigContext):
 
-    mvr_config: typing.Any
+    serial_numbers: list[tuple[str, str]]
 
 
 class MvrRigEtl(neuropixels_rig.NeuropixelsRigEtl):
@@ -24,23 +27,31 @@ class MvrRigEtl(neuropixels_rig.NeuropixelsRigEtl):
         self.mvr_config_source = mvr_config_source
 
     def _extract(self) -> ExtractContext:
+        mvr_config = utils.load_config(self.mvr_config_source)
+        serial_numbers = []
+        for mvr_name, assembly_name in self.mvr_mapping.items():
+            try:
+                mvr_camera_config = mvr_config[mvr_name]
+            except KeyError:
+                logger.debug(
+                    "No camera found for: %s in mvr config." %
+                    mvr_name
+                )
+            serial_numbers.append(
+                (
+                    assembly_name,
+                    "".join(mvr_camera_config["sn"]),
+                )
+            )
         return ExtractContext(
             current=super()._extract(),
-            mvr_config=utils.load_config(self.mvr_config_source),
+            serial_numbers=serial_numbers,
         )
 
     def _transform(
             self,
             extracted_source: ExtractContext) -> rig.Rig:
-        for mvr_name, assembly_name in self.mvr_mapping.items():
-            try:
-                mvr_camera_config = extracted_source.mvr_config[mvr_name]
-            except KeyError:
-                raise NeuropixelsRigException(
-                    "No camera found for: %s in mvr config." %
-                    mvr_name
-                )
-            serial_number = "".join(mvr_camera_config["sn"])
+        for assembly_name, serial_number in extracted_source.serial_numbers:
             utils.find_update(
                 extracted_source.current.cameras,
                 filters=[
